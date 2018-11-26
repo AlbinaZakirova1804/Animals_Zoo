6fe432d8be7ac1fa4d1e7beba400ce54e9f41460
github username <github email address>
2018-11-26 17:56:17 -0500

    finishes lab

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,29 @@
 class Animal
 
+attr_reader :species, :nickname, :zoo
+attr_accessor :weight
+
+@@animals=[]
+
+def initialize(species, weight, nickname, zoo)
+@species=species
+@nickname=nickname
+@weight=weight
+@@animals << self
+@zoo=zoo
+end
+
+
+def self.all
+  @@animals
+end
+
+def self.find_by_species(species)
+  self.all.map do |animal|
+    if animal.species == species
+      animal.nickname
+    end
+end.compact
+end
+
 end
